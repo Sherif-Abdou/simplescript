1859fc2bffa47c64085e2f23b8f3b68b3e0cb7de
Sherif Abdou <sherif_abdou@outlook.com>
2023-03-28 16:57:08 -0400

    It's actually starting to work somehow

--- src/parsing/new_expression_parser.rs
+++ src/parsing/new_expression_parser.rs
@@ -3,6 +3,7 @@ use crate::lexing::Token;
 use crate::parsing::sub_expression_parser::SubExpressionParser;
 use crate::parsing::ParsingResult;
 use std::cell::Cell;
+use std::collections::VecDeque;
 use std::iter::Skip;
 use std::ops::Index;
 use std::slice::Iter;
@@ -22,6 +23,14 @@ struct SlotList<'a> {
 }
 
 impl<'a> SlotList<'a> {
+    fn new(slots: &'a Vec<Slot>) -> Self {
+        Self {
+            internal_slots: slots,
+            start: Cell::new(0),
+            end: Cell::new(slots.len()),
+        }
+    }
+
     fn shift_by(&self, n: usize) {
         self.start.set(self.start.get() + n)
     }
@@ -63,7 +72,9 @@ impl<'a> Index<usize> for SlotList<'a> {
 
 #[derive(Default)]
 struct ExpressionParser<'a> {
+    raw: Vec<Token>,
     slots: Vec<Slot>,
+    consuming_token_stack: VecDeque<Token>,
     subparser: Option<Box<ExpressionParser<'a>>>,
     scope: Option<&'a dyn Scope>
 }
@@ -73,14 +84,87 @@ impl<'a> ExpressionParser<'a> {
         self.scope = scope;
         self
     }
+
+    /// Looks for slot within the same layer
     fn look_for(slot: Slot, slots: &SlotList) -> Option<usize> {
-        slots.iter().position(|s| slot.eq(s))
+        let mut layer : usize = 0;
+        for (i, current_slot) in slots.iter().enumerate() {
+            match current_slot {
+                Slot::Token(Token::OpenParenth)
+                | Slot::Token(Token::OpenSquare) => layer += 1,
+                Slot::Token(Token::CloseParenth)
+                | Slot::Token(Token::CloseSquare) => layer -= 1,
+                _ => {
+                    if *current_slot == slot {
+                        return Some(i as usize);
+                    }
+                }
+            };
+        }
+        None
+        // slots.iter().position(|s| slot.eq(s))
     }
+
+    fn handle_parentheses(&self, slots: &SlotList) -> Option<ExpressionEnum> {
+        let close_position = self.find_close_parenth(slots);
+        if close_position == -1 {return None;}
+
+        let subslots = slots.clone();
+        subslots.set_end_to(close_position as usize);
+        subslots.shift_by(1);
+
+        slots.shift_by((close_position + 1) as usize);
+        self.parse(&subslots)
+    }
+
+    fn find_close_parenth(&self, slots: &SlotList) -> i32 {
+        let mut layer = 0;
+        let mut close_position = -1;
+        for (position, slot) in slots.iter().enumerate() {
+            match *slot {
+                Slot::Token(Token::OpenParenth) => layer += 1,
+                Slot::Token(Token::CloseParenth) if layer > 1 => layer -= 1,
+                Slot::Token(Token::CloseParenth) if layer == 1 => close_position = position as i32,
+                _ => {}
+            }
+        }
+        close_position
+    }
+
+    fn handle_square_brackets(&self, slots: &SlotList) -> Option<ExpressionEnum> {
+        let close_position = self.find_close_square(slots);
+        if close_position == -1 { return None;}
+
+        let subslots = slots.clone();
+        subslots.set_end_to(close_position as usize);
+        subslots.shift_by(1);
+
+        slots.shift_by((close_position + 1) as usize);
+        let things = self.parse_comma_list(&subslots)?;
+        Some(ExpressionEnum::Array(things.into_iter().map(|v| v.into()).collect()))
+    }
+
+    fn find_close_square(&self, slots: &SlotList) -> i32 {
+        let mut layer = 0;
+        let mut close_position = -1;
+        for (position, slot) in slots.iter().enumerate() {
+            match *slot {
+                Slot::Token(Token::OpenSquare) => layer += 1,
+                Slot::Token(Token::CloseSquare) if layer > 1 => layer -= 1,
+                Slot::Token(Token::CloseSquare) if layer == 1 => close_position = position as i32,
+                _ => {}
+            }
+        }
+        close_position
+    }
+
     fn check_for_prefix(&self, slots: &SlotList) -> Option<ExpressionEnum> {
         // Look for unary operations
         let unary_type = match slots[0] {
             Slot::Token(Token::Ampersand) => Some(UnaryExpressionType::Reference),
             Slot::Token(Token::Star) => Some(UnaryExpressionType::Dereference),
+            Slot::Token(Token::OpenParenth) => return self.handle_parentheses(slots),
+            Slot::Token(Token::OpenSquare) => return self.handle_square_brackets(slots),
             _ => None,
         };
         if let Some(unary_type) = unary_type {
@@ -121,7 +205,17 @@ impl<'a> ExpressionParser<'a> {
             );
         }
         if scope.contains_function(identifier) {
-            assert_eq!(*slots.pop(), Slot::Token(Token::OpenParenth));
+            assert_eq!(slots[0], Slot::Token(Token::OpenParenth));
+            let close_position = self.find_close_parenth(slots);
+            let subsection = slots.clone();
+            slots.set_end_to(close_position as usize);
+            slots.shift_by(1);
+            let arguments = self.parse_comma_list(&subsection)?;
+            let expression = ExpressionEnum::FunctionCall(identifier.clone(),
+                arguments.iter().map(|v| (v.clone()).into()).collect());
+            return Some(
+                expression
+            );
         }
         None
     }
@@ -215,15 +309,45 @@ impl<'a> ExpressionParser<'a> {
 
 impl<'a> SubExpressionParser<'a> for ExpressionParser<'a> {
     fn consume(&mut self, token: Token) -> ParsingResult<bool> {
+        self.consume_token(token.clone());
+        match &token {
+            | Token::EOF
+            | Token::EOL
+            | Token::OpenCurly => self.consuming_token_stack.push_front(token.clone()),
+
+            Token::ClosedCurly
+            | Token::CloseSquare
+            | Token::CloseParenth => {
+                if self.consuming_token_stack.is_empty() {
+                    return Ok(false)
+                } else {
+                    self.consuming_token_stack.pop_front();
+                }
+            }
+            Token::Comma if self.consuming_token_stack.is_empty() => return Ok(false),
+            _ => return Ok(true)
+        }
+
+        Ok(true)
+    }
+
+    fn build(&mut self) -> Option<ExpressionEnum> {
+        let slotlist = SlotList::new(&self.slots);
+
+        self.parse(&slotlist)
+    }
+}
+
+impl<'a> ExpressionParser<'a> {
+    fn consume_token(&mut self, token: Token) {
         if let Some(ref mut subparser) = self.subparser {
-            let can_continue = subparser.consume(token.clone())?;
-            if !can_continue {
+            let can_continue = subparser.consume(token.clone());
+            if let Ok(false) = can_continue {
                 let built = subparser.build();
                 self.slots
                     .push(built.map(Slot::Expression).unwrap_or(Slot::None));
                 self.slots.push(Slot::Token(token.clone()));
                 self.subparser = None;
-                return Ok(true);
             }
         }
 
@@ -241,6 +365,7 @@ impl<'a> SubExpressionParser<'a> for ExpressionParser<'a> {
                 .slots
                 .push(Slot::Expression(ExpressionEnum::FloatLiteral(float))),
             Token::OpenSquare | Token::OpenParenth => {
+                self.slots.push(Slot::Token(token));
                 self.subparser = Some(Box::new(ExpressionParser::default().set_scope(self.scope)))
             }
             // Weird case with comma, need to work with that
@@ -248,16 +373,47 @@ impl<'a> SubExpressionParser<'a> for ExpressionParser<'a> {
             | Token::EOF
             | Token::EOL
             | Token::ClosedCurly
-            | Token::CloseSquare
-            | Token::CloseParenth
-            | Token::OpenCurly => return Ok(false),
+            // | Token::CloseSquare
+            // | Token::CloseParenth
+            | Token::OpenCurly => {},
             token => self.slots.push(Slot::Token(token)),
         };
+    }
+}
 
-        Ok(true)
+#[cfg(test)]
+mod test {
+    use crate::lexing::Token;
+    use crate::parsing::new_expression_parser::ExpressionParser;
+    use crate::parsing::sub_expression_parser::SubExpressionParser;
+
+    #[test]
+    fn test_basic_binary() {
+        let tokens = vec![Token::Integer(2), Token::Plus, Token::Integer(3)];
+        print_parsed(tokens);
     }
 
-    fn build(&mut self) -> Option<ExpressionEnum> {
-        todo!()
+    #[test]
+    fn test_advanced_binary() {
+        let tokens = vec![Token::Integer(2), Token::Plus, Token::Integer(3), Token::Slash, Token::Integer(78)];
+        print_parsed(tokens);
+    }
+
+    #[test]
+    fn test_array() {
+        let tokens = vec![Token::OpenSquare, Token::Integer(3), Token::Comma, Token::Float(2.7), Token::CloseSquare];
+        print_parsed(tokens);
+    }
+
+    fn print_parsed(tokens: Vec<Token>) {
+        let mut parser = ExpressionParser::default();
+
+        for token in tokens {
+            parser.consume(token).unwrap();
+        }
+
+        let expr = parser.build();
+
+        println!("{:?}", expr);
     }
 }
